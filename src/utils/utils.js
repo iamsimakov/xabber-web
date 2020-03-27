20975cca43909b4bce458c5323376250d03d5d46
Valeria Titova <valeria.nogina@redsolution.com>
2020-03-27 15:32:35 +0500

    Added fromCodePoint realization for unsupporting browsers

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -10,6 +10,70 @@ define([
         _ = deps._,
         moment = deps.moment;
 
+    // String extensions
+    /*! http://mths.be/fromcodepoint v0.1.0 by @mathias */
+    if (!String.fromCodePoint) {
+        (function() {
+            var defineProperty = (function() {
+                // IE 8 поддерживает метод `Object.defineProperty` только на элементах DOM
+                try {
+                    var object = {};
+                    var $defineProperty = Object.defineProperty;
+                    var result = $defineProperty(object, object, object) && $defineProperty;
+                } catch(error) {}
+                return result;
+            }());
+            var stringFromCharCode = String.fromCharCode;
+            var floor = Math.floor;
+            var fromCodePoint = function() {
+                var MAX_SIZE = 0x4000;
+                var codeUnits = [];
+                var highSurrogate;
+                var lowSurrogate;
+                var index = -1;
+                var length = arguments.length;
+                if (!length) {
+                    return '';
+                }
+                var result = '';
+                while (++index < length) {
+                    var codePoint = Number(arguments[index]);
+                    if (
+                        !isFinite(codePoint) ||       // `NaN`, `+Infinity` или `-Infinity`
+                        codePoint < 0 ||              // неверная кодовая точка Юникода
+                        codePoint > 0x10FFFF ||       // неверная кодовая точка Юникода
+                        floor(codePoint) != codePoint // не целое число
+                    ) {
+                        throw RangeError('Invalid code point: ' + codePoint);
+                    }
+                    if (codePoint <= 0xFFFF) { // кодовая точка Базовой многоязыковой плоскости (БМП)
+                        codeUnits.push(codePoint);
+                    } else { // Астральная кодовая точка; делим её на суррогатную пару
+                        // http://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
+                        codePoint -= 0x10000;
+                        highSurrogate = (codePoint >> 10) + 0xD800;
+                        lowSurrogate = (codePoint % 0x400) + 0xDC00;
+                        codeUnits.push(highSurrogate, lowSurrogate);
+                    }
+                    if (index + 1 == length || codeUnits.length > MAX_SIZE) {
+                        result += stringFromCharCode.apply(null, codeUnits);
+                        codeUnits.length = 0;
+                    }
+                }
+                return result;
+            };
+            if (defineProperty) {
+                defineProperty(String, 'fromCodePoint', {
+                    'value': fromCodePoint,
+                    'configurable': true,
+                    'writable': true
+                });
+            } else {
+                String.fromCodePoint = fromCodePoint;
+            }
+        }());
+    };
+
     // jQuery extensions
     $.fn.switchClass = function (klass, condition) {
         if (arguments.length === 1) {
